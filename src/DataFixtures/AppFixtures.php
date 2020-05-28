022f224ffd830802f284ed067b5c450ebbe0da08
bociek <37113026+bociek@users.noreply.github.com>
2020-05-28 14:10:48 +0200

    Add files via upload

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * App fixtures.
+ */
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+/**
+ * Class AppFixtures
+ * @package App\DataFixtures
+ */
+class AppFixtures extends Fixture
+{
+    /**
+     * Load.
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
